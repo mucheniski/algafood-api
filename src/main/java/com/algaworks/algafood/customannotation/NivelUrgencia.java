9c570bd97c9f156b3c89505782ba87247e0e3671
mucheniski <mucheniski@gmail.com>
2020-02-06 16:50:33 -0300

    Classes exemplo deletadas, projeto limpo aula 3.3

--- src/main/java/com/algaworks/algafood/customannotation/NivelUrgencia.java
+++
@@ -1,6 +0,0 @@
-package com.algaworks.algafood.customannotation;
-
-public enum NivelUrgencia {
-	URGENTE, 
-	NORMAL;
-}
--- src/main/java/com/algaworks/algafood/notification/Notificador.java
+++
@@ -1,9 +0,0 @@
-package com.algaworks.algafood.notification;
-
-import com.algaworks.algafood.domain.Cliente;
-
-public interface Notificador {
-
-	void notificar(Cliente cliente, String mensagem);
-
-}
