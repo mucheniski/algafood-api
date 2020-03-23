672ef50d7d7cfd03b06caca9a30cf12b10b5bc48
mucheniski <mucheniski@gmail.com>
2020-03-23 18:32:36 -0300

    5.10. Externalizando consultas JPQL para um arquivo XML

--- src/main/java/com/algaworks/algafood/repository/RestauranteRepository.java
+++ src/main/java/com/algaworks/algafood/repository/RestauranteRepository.java
@@ -4,7 +4,6 @@ import java.math.BigDecimal;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
@@ -17,7 +16,9 @@ public interface RestauranteRepository extends JpaRepository<Restaurante, Long> 
 	
 	// List<Restaurante> findByNomeContainingAndCozinhaId(String nome, Long cozinhaId);
 	
-	@Query("FROM Restaurante WHERE nome LIKE %:nome% AND cozinha.id = :cozinhaId" )
-	List<Restaurante> consultarPorNome(String nome, @Param("cozinhaId") Long cozinhaId); // O nome não precisa do @Param pois como é o mesmo nome do atributo o spring data já faz o binding
+//	@Query("FROM Restaurante WHERE nome LIKE %:nome% AND cozinha.id = :cozinhaId" )
+//	List<Restaurante> consultarPorNome(String nome, @Param("cozinhaId") Long cozinhaId); // O nome não precisa do @Param pois como é o mesmo nome do atributo o spring data já faz o binding
+
+	List<Restaurante> consultarPorNome(String nome, @Param("cozinhaId") Long cozinhaId);
 	
 }
