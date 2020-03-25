5912109f6ec0043a54c879f766f7907d7f1ee32e
mucheniski <mucheniski@gmail.com>
2020-03-25 20:23:19 -0300

    5.12. Implementando uma consulta dinâmica com JPQL.mp4

--- src/main/java/com/algaworks/algafood/repository/RestauranteRepositoryImpl.java
+++ src/main/java/com/algaworks/algafood/repository/RestauranteRepositoryImpl.java
@@ -1,12 +1,16 @@
 package com.algaworks.algafood.repository;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
 import com.algaworks.algafood.entity.Restaurante;
 
@@ -19,15 +23,29 @@ public class RestauranteRepositoryImpl implements RestauranteRepositoryQueries {
 	@Override
 	public List<Restaurante> findByNomeTaxaFrete(String nome, BigDecimal taxaInicial, BigDecimal taxaFinal) {
 		
-		String jpql = "from Restaurante "
-					+ " where nome like :nome "
-					+ " and taxaFrete between :taxaInicial and :taxaFinal";
+		StringBuilder jpql = new StringBuilder();
+		jpql.append("from Restaurante where 0 = 0 ");
 		
-		return manager.createQuery(jpql, Restaurante.class)
-						.setParameter("nome", "%" + nome + "%")
-						.setParameter("taxaInicial", taxaInicial)
-						.setParameter("taxaFinal", taxaFinal)
-						.getResultList();
+		Map<String, Object> parametros = new HashMap<>();
+		
+		if (StringUtils.hasLength(nome)) {
+			jpql.append("and nome like :nome ");
+			parametros.put("nome", "%" + nome + "%");
+		}
+		
+		if (taxaInicial != null) {
+			jpql.append("and taxaFrete >= :taxaInicial ");
+			parametros.put("taxaInicial", taxaInicial);
+		}
+		
+		if (taxaFinal != null) {
+			jpql.append("and taxaFrete <= :taxaFinal ");
+			parametros.put("taxaFinal", taxaFinal);
+		}
+		
+		TypedQuery<Restaurante> query = manager.createQuery(jpql.toString(), Restaurante.class);		
+		parametros.forEach((chave, valor) -> query.setParameter(chave, valor));
+		return query.getResultList();
 		
 	}
 	
