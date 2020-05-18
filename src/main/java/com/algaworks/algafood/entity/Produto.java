7c06a1180feff1ad1a92eec8a0b62c8b0e624c0d
mucheniski <mucheniski@gmail.com>
2020-05-18 18:15:54 -0300

    6.7. Desafio mapeando relacionamento muitos-para-um

---
+++ src/main/java/com/algaworks/algafood/entity/Produto.java
@@ -0,0 +1,38 @@
+package com.algaworks.algafood.entity;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true) // Explicaçao https://projectlombok.org/features/EqualsAndHashCode
+public class Produto {
+	
+	@EqualsAndHashCode.Include
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false)
+	private String nome;
+	
+	@Column(nullable = false)
+	private String descricao;
+	
+	@Column(nullable = false)
+	private BigDecimal preco;
+	
+	@Column(nullable = false)
+	private Boolean ativo;
+	
+	@ManyToOne
+	private Restaurante rastaurante;
+
+}
