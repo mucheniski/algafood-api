409b5720aa8fdc26d8e51b8fcf9d3025c07b23f5
mucheniski <mucheniski@gmail.com>
2020-07-06 18:27:57 -0300

    7.6. Adicionando o Flyway no projeto e criando a primeira migração

--- src/main/java/com/algaworks/algafood/entity/Cozinha.java
+++ src/main/java/com/algaworks/algafood/entity/Cozinha.java
@@ -29,9 +29,6 @@ public class Cozinha {
 	
 	@Column(nullable = false)
 	private String nome;
-		
-	@Column(name = "observacao", nullable = false)
-	private String descricao;
 	
 	@JsonIgnore
 	@OneToMany(mappedBy = "cozinha")
