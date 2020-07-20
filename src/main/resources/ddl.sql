68362320e4599307cf623c8d44e43ab4881f053f
mucheniski <mucheniski@gmail.com>
2020-07-20 18:47:45 -0300

    7.9. Criando migração a partir de DDL gerado por schema generation

---
+++ src/main/resources/ddl.sql
@@ -0,0 +1,88 @@
+create table cidade (id bigint not null auto_increment, nome varchar(255) not null, estado_id bigint, primary key (id)) engine=InnoDB
+create table cozinha (id bigint not null auto_increment, nome varchar(255) not null, primary key (id)) engine=InnoDB
+create table estado (id bigint not null auto_increment, nome varchar(255) not null, primary key (id)) engine=InnoDB
+create table forma_pagamento (id bigint not null auto_increment, descricao varchar(255) not null, primary key (id)) engine=InnoDB
+create table grupo (id bigint not null auto_increment, nome varchar(255) not null, primary key (id)) engine=InnoDB
+create table grupo_permissao (grupo_id bigint not null, permissao_id bigint not null) engine=InnoDB
+create table permissao (id bigint not null auto_increment, descricao varchar(255) not null, nome varchar(255) not null, primary key (id)) engine=InnoDB
+create table produto (id bigint not null auto_increment, ativo bit not null, descricao varchar(255) not null, nome varchar(255) not null, preco decimal(19,2) not null, restaurante_id bigint not null, primary key (id)) engine=InnoDB
+create table restaurante (id bigint not null auto_increment, data_atualizacao datetime not null, data_cadastro datetime not null, bairro varchar(255), cep varchar(255), complemento varchar(255), logradouro varchar(255), numero varchar(255), nome varchar(255) not null, taxa_frete decimal(19,2) not null, cozinha_id bigint not null, cidade_id bigint, primary key (id)) engine=InnoDB
+create table restaurante_forma_pagamento (rastaurante_id bigint not null, forma_pagamento_id bigint not null) engine=InnoDB
+create table usuario (id bigint not null auto_increment, data_cadastro datetime not null, email varchar(255) not null, nome varchar(255) not null, senha varchar(255) not null, primary key (id)) engine=InnoDB
+create table usuario_grupo (usuario_id bigint not null, grupo_id bigint not null) engine=InnoDB
+alter table cidade add constraint FKkworrwk40xj58kevvh3evi500 foreign key (estado_id) references estado (id)
+alter table grupo_permissao add constraint FKh21kiw0y0hxg6birmdf2ef6vy foreign key (permissao_id) references permissao (id)
+alter table grupo_permissao add constraint FKta4si8vh3f4jo3bsslvkscc2m foreign key (grupo_id) references grupo (id)
+alter table produto add constraint FKb9jhjyghjcn25guim7q4pt8qx foreign key (restaurante_id) references restaurante (id)
+alter table restaurante add constraint FK76grk4roudh659skcgbnanthi foreign key (cozinha_id) references cozinha (id)
+alter table restaurante add constraint FKnbrdf9c9165xgwuynayr85m0h foreign key (cidade_id) references cidade (id)
+alter table restaurante_forma_pagamento add constraint FK7aln770m80358y4olr03hyhh2 foreign key (forma_pagamento_id) references forma_pagamento (id)
+alter table restaurante_forma_pagamento add constraint FKjqgyhck94uji4gyurw0yijovk foreign key (rastaurante_id) references restaurante (id)
+alter table usuario_grupo add constraint FKk30suuy31cq5u36m9am4om9ju foreign key (grupo_id) references grupo (id)
+alter table usuario_grupo add constraint FKdofo9es0esuiahyw2q467crxw foreign key (usuario_id) references usuario (id)
+create table cidade (id bigint not null auto_increment, nome varchar(255) not null, estado_id bigint, primary key (id)) engine=InnoDB
+create table cozinha (id bigint not null auto_increment, nome varchar(255) not null, primary key (id)) engine=InnoDB
+create table estado (id bigint not null auto_increment, nome varchar(255) not null, primary key (id)) engine=InnoDB
+create table forma_pagamento (id bigint not null auto_increment, descricao varchar(255) not null, primary key (id)) engine=InnoDB
+create table grupo (id bigint not null auto_increment, nome varchar(255) not null, primary key (id)) engine=InnoDB
+create table grupo_permissao (grupo_id bigint not null, permissao_id bigint not null) engine=InnoDB
+create table permissao (id bigint not null auto_increment, descricao varchar(255) not null, nome varchar(255) not null, primary key (id)) engine=InnoDB
+create table produto (id bigint not null auto_increment, ativo bit not null, descricao varchar(255) not null, nome varchar(255) not null, preco decimal(19,2) not null, restaurante_id bigint not null, primary key (id)) engine=InnoDB
+create table restaurante (id bigint not null auto_increment, data_atualizacao datetime not null, data_cadastro datetime not null, bairro varchar(255), cep varchar(255), complemento varchar(255), logradouro varchar(255), numero varchar(255), nome varchar(255) not null, taxa_frete decimal(19,2) not null, cozinha_id bigint not null, cidade_id bigint, primary key (id)) engine=InnoDB
+create table restaurante_forma_pagamento (rastaurante_id bigint not null, forma_pagamento_id bigint not null) engine=InnoDB
+create table usuario (id bigint not null auto_increment, data_cadastro datetime not null, email varchar(255) not null, nome varchar(255) not null, senha varchar(255) not null, primary key (id)) engine=InnoDB
+create table usuario_grupo (usuario_id bigint not null, grupo_id bigint not null) engine=InnoDB
+alter table cidade add constraint FKkworrwk40xj58kevvh3evi500 foreign key (estado_id) references estado (id)
+alter table grupo_permissao add constraint FKh21kiw0y0hxg6birmdf2ef6vy foreign key (permissao_id) references permissao (id)
+alter table grupo_permissao add constraint FKta4si8vh3f4jo3bsslvkscc2m foreign key (grupo_id) references grupo (id)
+alter table produto add constraint FKb9jhjyghjcn25guim7q4pt8qx foreign key (restaurante_id) references restaurante (id)
+alter table restaurante add constraint FK76grk4roudh659skcgbnanthi foreign key (cozinha_id) references cozinha (id)
+alter table restaurante add constraint FKnbrdf9c9165xgwuynayr85m0h foreign key (cidade_id) references cidade (id)
+alter table restaurante_forma_pagamento add constraint FK7aln770m80358y4olr03hyhh2 foreign key (forma_pagamento_id) references forma_pagamento (id)
+alter table restaurante_forma_pagamento add constraint FKjqgyhck94uji4gyurw0yijovk foreign key (rastaurante_id) references restaurante (id)
+alter table usuario_grupo add constraint FKk30suuy31cq5u36m9am4om9ju foreign key (grupo_id) references grupo (id)
+alter table usuario_grupo add constraint FKdofo9es0esuiahyw2q467crxw foreign key (usuario_id) references usuario (id)
+create table cidade (id bigint not null auto_increment, nome varchar(255) not null, estado_id bigint, primary key (id)) engine=InnoDB
+create table cozinha (id bigint not null auto_increment, nome varchar(255) not null, primary key (id)) engine=InnoDB
+create table estado (id bigint not null auto_increment, nome varchar(255) not null, primary key (id)) engine=InnoDB
+create table forma_pagamento (id bigint not null auto_increment, descricao varchar(255) not null, primary key (id)) engine=InnoDB
+create table grupo (id bigint not null auto_increment, nome varchar(255) not null, primary key (id)) engine=InnoDB
+create table grupo_permissao (grupo_id bigint not null, permissao_id bigint not null) engine=InnoDB
+create table permissao (id bigint not null auto_increment, descricao varchar(255) not null, nome varchar(255) not null, primary key (id)) engine=InnoDB
+create table produto (id bigint not null auto_increment, ativo bit not null, descricao varchar(255) not null, nome varchar(255) not null, preco decimal(19,2) not null, restaurante_id bigint not null, primary key (id)) engine=InnoDB
+create table restaurante (id bigint not null auto_increment, data_atualizacao datetime not null, data_cadastro datetime not null, bairro varchar(255), cep varchar(255), complemento varchar(255), logradouro varchar(255), numero varchar(255), nome varchar(255) not null, taxa_frete decimal(19,2) not null, cozinha_id bigint not null, cidade_id bigint, primary key (id)) engine=InnoDB
+create table restaurante_forma_pagamento (rastaurante_id bigint not null, forma_pagamento_id bigint not null) engine=InnoDB
+create table usuario (id bigint not null auto_increment, data_cadastro datetime not null, email varchar(255) not null, nome varchar(255) not null, senha varchar(255) not null, primary key (id)) engine=InnoDB
+create table usuario_grupo (usuario_id bigint not null, grupo_id bigint not null) engine=InnoDB
+alter table cidade add constraint FKkworrwk40xj58kevvh3evi500 foreign key (estado_id) references estado (id)
+alter table grupo_permissao add constraint FKh21kiw0y0hxg6birmdf2ef6vy foreign key (permissao_id) references permissao (id)
+alter table grupo_permissao add constraint FKta4si8vh3f4jo3bsslvkscc2m foreign key (grupo_id) references grupo (id)
+alter table produto add constraint FKb9jhjyghjcn25guim7q4pt8qx foreign key (restaurante_id) references restaurante (id)
+alter table restaurante add constraint FK76grk4roudh659skcgbnanthi foreign key (cozinha_id) references cozinha (id)
+alter table restaurante add constraint FKnbrdf9c9165xgwuynayr85m0h foreign key (cidade_id) references cidade (id)
+alter table restaurante_forma_pagamento add constraint FK7aln770m80358y4olr03hyhh2 foreign key (forma_pagamento_id) references forma_pagamento (id)
+alter table restaurante_forma_pagamento add constraint FKjqgyhck94uji4gyurw0yijovk foreign key (rastaurante_id) references restaurante (id)
+alter table usuario_grupo add constraint FKk30suuy31cq5u36m9am4om9ju foreign key (grupo_id) references grupo (id)
+alter table usuario_grupo add constraint FKdofo9es0esuiahyw2q467crxw foreign key (usuario_id) references usuario (id)
+create table cidade (id bigint not null auto_increment, nome varchar(255) not null, estado_id bigint, primary key (id)) engine=InnoDB
+create table cozinha (id bigint not null auto_increment, nome varchar(255) not null, primary key (id)) engine=InnoDB
+create table estado (id bigint not null auto_increment, nome varchar(255) not null, primary key (id)) engine=InnoDB
+create table forma_pagamento (id bigint not null auto_increment, descricao varchar(255) not null, primary key (id)) engine=InnoDB
+create table grupo (id bigint not null auto_increment, nome varchar(255) not null, primary key (id)) engine=InnoDB
+create table grupo_permissao (grupo_id bigint not null, permissao_id bigint not null) engine=InnoDB
+create table permissao (id bigint not null auto_increment, descricao varchar(255) not null, nome varchar(255) not null, primary key (id)) engine=InnoDB
+create table produto (id bigint not null auto_increment, ativo bit not null, descricao varchar(255) not null, nome varchar(255) not null, preco decimal(19,2) not null, restaurante_id bigint not null, primary key (id)) engine=InnoDB
+create table restaurante (id bigint not null auto_increment, data_atualizacao datetime not null, data_cadastro datetime not null, bairro varchar(255), cep varchar(255), complemento varchar(255), logradouro varchar(255), numero varchar(255), nome varchar(255) not null, taxa_frete decimal(19,2) not null, cozinha_id bigint not null, cidade_id bigint, primary key (id)) engine=InnoDB
+create table restaurante_forma_pagamento (rastaurante_id bigint not null, forma_pagamento_id bigint not null) engine=InnoDB
+create table usuario (id bigint not null auto_increment, data_cadastro datetime not null, email varchar(255) not null, nome varchar(255) not null, senha varchar(255) not null, primary key (id)) engine=InnoDB
+create table usuario_grupo (usuario_id bigint not null, grupo_id bigint not null) engine=InnoDB
+alter table cidade add constraint FKkworrwk40xj58kevvh3evi500 foreign key (estado_id) references estado (id)
+alter table grupo_permissao add constraint FKh21kiw0y0hxg6birmdf2ef6vy foreign key (permissao_id) references permissao (id)
+alter table grupo_permissao add constraint FKta4si8vh3f4jo3bsslvkscc2m foreign key (grupo_id) references grupo (id)
+alter table produto add constraint FKb9jhjyghjcn25guim7q4pt8qx foreign key (restaurante_id) references restaurante (id)
+alter table restaurante add constraint FK76grk4roudh659skcgbnanthi foreign key (cozinha_id) references cozinha (id)
+alter table restaurante add constraint FKnbrdf9c9165xgwuynayr85m0h foreign key (cidade_id) references cidade (id)
+alter table restaurante_forma_pagamento add constraint FK7aln770m80358y4olr03hyhh2 foreign key (forma_pagamento_id) references forma_pagamento (id)
+alter table restaurante_forma_pagamento add constraint FKjqgyhck94uji4gyurw0yijovk foreign key (rastaurante_id) references restaurante (id)
+alter table usuario_grupo add constraint FKk30suuy31cq5u36m9am4om9ju foreign key (grupo_id) references grupo (id)
+alter table usuario_grupo add constraint FKdofo9es0esuiahyw2q467crxw foreign key (usuario_id) references usuario (id)
