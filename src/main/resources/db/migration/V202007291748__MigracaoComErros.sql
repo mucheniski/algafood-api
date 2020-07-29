9801654238e8b9d8857208628a47ac4072f1af4b
mucheniski <mucheniski@gmail.com>
2020-07-29 18:19:33 -0300

    7.11. Reparando migrações com erros

---
+++ src/main/resources/db/migration/V202007291748__MigracaoComErros.sql
@@ -0,0 +1,6 @@
+-- algafood2.falha definition
+
+CREATE TABLE `falha` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
