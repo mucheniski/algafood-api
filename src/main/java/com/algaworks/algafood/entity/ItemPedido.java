4daef1e89000a9e98cd42674e32be739df8cd0c7
mucheniski <mucheniski@gmail.com>
2020-08-07 20:19:52 -0300

    7.12 Desafio Pedido ItemPedido parte2 - Mapeamento JPA

--- src/main/java/com/algaworks/algafood/entity/ItemPedido.java
+++ src/main/java/com/algaworks/algafood/entity/ItemPedido.java
@@ -2,15 +2,44 @@ package com.algaworks.algafood.entity;
 
 import java.math.BigDecimal;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
 public class ItemPedido {
 	
+	@EqualsAndHashCode.Include
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false)
 	private Integer quantidade;
+
+	@Column(nullable = false)
 	private BigDecimal precoUnitario;
+	
+	@Column(nullable = false)
 	private BigDecimal precoTotal;
+	
 	private String observacao;
 	
+	@ManyToOne
+	@JoinColumn(name = "produto_id", nullable = false)
 	private Produto produto;
 	
+	@ManyToOne
+	@JoinColumn(name = "pedido_id", nullable = false)
 	private Pedido pedido;
 
 }
--- src/main/java/com/algaworks/algafood/entity/Pedido.java
+++ src/main/java/com/algaworks/algafood/entity/Pedido.java
@@ -5,26 +5,69 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+import org.hibernate.annotations.CreationTimestamp;
+
 import com.algaworks.algafood.entity.enuns.StatusPedido;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
 public class Pedido {
 	
+	@EqualsAndHashCode.Include
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false)
 	private BigDecimal subtotal;
+	
+	@Column(nullable = false)
 	private BigDecimal taxaFrete;
+	
+	@Column(nullable = false)
 	private BigDecimal valorTotal;
+	
+	@CreationTimestamp
+	@Column(nullable = false, columnDefinition = "datetime")
 	private LocalDateTime dataCriacao;
+	
 	private LocalDateTime dataConfirmacao;
 	private LocalDateTime dataCancelamento;
 	private LocalDateTime dataEntrega;
 	
-	private Endereco endereco;
+	@ManyToOne
+	@JoinColumn(name = "usuario_cliente_id", nullable = false)
+	private Usuario usuarioCliente;
 	
+	@Embedded
+	@Column(nullable = false)
+	private Endereco enderecoEntrega;
+	
+	@ManyToOne
+	@JoinColumn(nullable = false)
 	private Restaurante restaurante;
 		
+	@ManyToOne
+	@JoinColumn(nullable = false)
 	private FormaPagamento formaPagamento;
 	
 	private StatusPedido status;
 	
-	private List itens = new ArrayList<>();
+	@OneToMany(mappedBy = "pedido")
+	private List<ItemPedido> itens = new ArrayList<>();
 
 }
