9c6e38e8f7ee0e5f8f1965a1fc5f430af1c1f90e
mucheniski <mucheniski@gmail.com>
2021-03-09 09:38:39 -0300

    15.13. Reagindo a Domain Events em fases específicas da transação

--- src/main/java/com/algaworks/algafood/event/PedidoConfirmadoListener.java
+++ src/main/java/com/algaworks/algafood/event/PedidoConfirmadoListener.java
@@ -7,6 +7,8 @@ import lombok.var;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
 
 @Component
 public class PedidoConfirmadoListener {
@@ -18,8 +20,15 @@ public class PedidoConfirmadoListener {
         O método é chamado automáticamente pelo spring pois fica ouvindo os eventos nesse caso esse evento é disparado ao
         ser executado o repository.save(pedido) do PedidoService, porque na classe pedido no método status confirmado o
         evento é registrado registerEvent(new PedidoConfirmadoEvent(this));
+
+        TransactionanEventListener serve para que possamos indicar em qual momento da transação queremos que o evento seja disparado
+        por padrão, se não colocarmos nada na frente é disparado após o commit
+
+        Nesse caso informado o BEFORE_COMMIT o método só é executado antes de ser feito o commit, caso de algum erro é feito rollback
+        Aí fica a critério da necessidade da api, caso o método a ser chamado não seja tão importante pode ser deixado o padrão.
+        como no exemplo abaixo é só um envio de email, poderia ser deixado sem phase que seria a default AFTER_COMMIT
      */
-    @EventListener
+    @TransactionalEventListener(phase = TransactionPhase.BEFORE_COMMIT)
     public void pedidoConfirmado(PedidoConfirmadoEvent pedidoConfirmadoEvent) {
 
         Pedido pedido = pedidoConfirmadoEvent.getPedido();
