d90f7ff5d10699c6e84fd7eb52a4432a63cc89a5
mucheniski <mucheniski@gmail.com>
2021-03-24 13:58:57 -0300

    18.8. Descrevendo as operações de endpoints na documentação

--- src/main/java/com/algaworks/algafood/controller/CidadeController.java
+++ src/main/java/com/algaworks/algafood/controller/CidadeController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.validation.Valid;
 
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -28,27 +29,32 @@ public class CidadeController {
 	@Autowired
 	private CidadeService service;
 
+	@ApiOperation("Lista as cidades")
 	@GetMapping
 	public List<CidadeDTO> listar() {
 		return service.listar();
 	}
 
+	@ApiOperation("Busca uma cidade por id")
 	@GetMapping("/{id}")
 	public CidadeDTO buscarPorId(@PathVariable Long id) {
 		return service.buscarDtoPorId(id);
 	}
 
+	@ApiOperation("Cadastra uma cidade")
 	@ResponseStatus(HttpStatus.CREATED)
 	@PostMapping
 	public CidadeDTO salvar(@RequestBody @Valid CidadeDTO dto) {
 		return service.salvar(dto);
 	}
 
+	@ApiOperation("Atualiza uma cidade por id")
 	@PutMapping("/{id}")
 	public CidadeDTO atualizar(@PathVariable Long id, @RequestBody @Valid CidadeDTO dto) {
 		return service.atualizar(id, dto);
 	}
 
+	@ApiOperation("Exclui uma cidade por ai")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
 	@DeleteMapping("/{id}")
 	public void remover(@PathVariable Long id) {
