0889ec0adaac21ee6fd528e620b325ae85ed589d
mucheniski <mucheniski@gmail.com>
2021-03-24 13:56:04 -0300

    18.7. Descrevendo tags na documentação e associando com controllers

--- src/main/java/com/algaworks/algafood/controller/CidadeController.java
+++ src/main/java/com/algaworks/algafood/controller/CidadeController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import javax.validation.Valid;
 
+import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -19,6 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.algaworks.algafood.dto.CidadeDTO;
 import com.algaworks.algafood.service.CidadeService;
 
+@Api(tags = "Cidades")
 @RestController
 @RequestMapping("/cidades")
 public class CidadeController {
--- src/main/java/com/algaworks/algafood/documentation/SpringFoxConfig.java
+++ src/main/java/com/algaworks/algafood/documentation/SpringFoxConfig.java
@@ -8,6 +8,7 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.Tag;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -30,7 +31,8 @@ public class SpringFoxConfig implements WebMvcConfigurer {
                             .apis(RequestHandlerSelectors.basePackage("com.algaworks.algafood"))
 //                            .paths(PathSelectors.ant("/restaurantes/**")) // Caso queira buscar de endpoints específicos apenas
                    .build()
-                .apiInfo(customApiInfo());
+                .apiInfo(customApiInfo())
+                .tags(new Tag("Cidades", "Gerencia as cidades"));
     }
 
     /*
