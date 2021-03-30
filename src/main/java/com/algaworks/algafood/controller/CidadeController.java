493bb666f7235aebac35753b750ef0999c883846
mucheniski <mucheniski@gmail.com>
2021-03-30 08:46:13 -0300

    18.16. Descrevendo códigos de status de respostas em endpoints específicos

--- src/main/java/com/algaworks/algafood/controller/CidadeController.java
+++ src/main/java/com/algaworks/algafood/controller/CidadeController.java
@@ -4,9 +4,8 @@ import java.util.List;
 
 import javax.validation.Valid;
 
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
+import com.algaworks.algafood.exception.Problema;
+import io.swagger.annotations.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -38,6 +37,14 @@ public class CidadeController {
 
 	@ApiOperation("Busca uma cidade por id")
 	@GetMapping("/{id}")
+	/*
+	* Quando é algúm retorno mais específico do método e não é tratado de forma Global no SpringFoxConfig, deve ser implementado diretamente no controller com
+	* o @ApiRresponse que recebe um array de responses
+	* */
+	@ApiResponses({
+		@ApiResponse(code = 400, message = "Parametro passado inválido", response = Problema.class),
+		@ApiResponse(code = 404, message = "Cidade não encontrada", response = Problema.class)
+	})
 	public CidadeDTO buscarPorId(
 			@ApiParam(value = "id de uma cidade", example = "1") @PathVariable Long id
 	) {
@@ -47,6 +54,12 @@ public class CidadeController {
 	@ApiOperation("Cadastra uma cidade")
 	@ResponseStatus(HttpStatus.CREATED)
 	@PostMapping
+	/*
+	* No caso do 201 created não precisamos colocar response porque o retorno do método já é uma CidadeDTO, então o SpringFox já sabe o que retornar
+	* */
+	@ApiResponses({
+		@ApiResponse(code = 201, message = "Criado com sucesso")
+	})
 	public CidadeDTO salvar(
 			@ApiParam(name = "corpo", value = "Representação de uma nova cidade") @RequestBody @Valid CidadeDTO dto
 	) {
@@ -55,6 +68,10 @@ public class CidadeController {
 
 	@ApiOperation("Atualiza uma cidade por id")
 	@PutMapping("/{id}")
+	@ApiResponses({
+		@ApiResponse(code = 200, message = "Atualizado com sucesso"),
+		@ApiResponse(code = 404, message = "Não encontrado com o id", response = Problema.class)
+	})
 	public CidadeDTO atualizar(
 			@ApiParam(value = "id de uma cidade", example = "1") @PathVariable Long id,
 			@ApiParam(name = "corpo", value = "Representação de uma cidade atualizada") @RequestBody @Valid CidadeDTO dto
@@ -65,6 +82,10 @@ public class CidadeController {
 	@ApiOperation("Exclui uma cidade por ai")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
 	@DeleteMapping("/{id}")
+	@ApiResponses({
+		@ApiResponse(code = 204, message = "Excluído com sucesso"),
+		@ApiResponse(code = 404, message = "Não encontrado com o id", response = Problema.class)
+	})
 	public void remover(
 			@ApiParam(value = "id de uma cidade", example = "1") @PathVariable Long id
 	) {
